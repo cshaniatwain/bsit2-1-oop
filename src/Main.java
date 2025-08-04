6dd6b5aa1b6d089e76731ee39bdca73b0db413a5
Shania Twain <stcastro44184@liceo.edu.ph>
2025-08-04 10:16:08 +0800

    Update Main.java
    Thank you sir!

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,36 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int totalNumber = 0;
-        for (int i = 1; i <= 5; i++) {
-            System.out.print("Enter number " + i + ": ");
-            int number = scanner.nextInt();
-            totalNumber += number; 
+public class Book {
+    
+      String title;
+      String author;
+       int pages;
+      boolean isAvailable;
+      
+    public Book(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isAvailable = true; 
+        System.out.println("A new book '" + title + "' by [" + author + "] has been added to the library!");
+    }
+    public void displayInfo() {
+        System.out.println("Book Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pages);
+        System.out.println("Available for Borrowing: " + (isAvailable ? "Yes" : "No"));
+        System.out.println();
+    } 
+    public void borrowBook() {
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println("You have borrowed '" + title + "'. Enjoy reading!");
+        } else {
+            System.out.println("Sorry, '" + title + "' is currently not available for borrowing.");
         }
-        System.out.println("Total Number: " + totalNumber);
-        scanner.close();
+    }
+    public static void main(String[] args) {
+        Book book1 = new Book("1984", "George Orwell", 328);
+        book1.displayInfo();
+        book1.borrowBook();
+        book1.displayInfo();
     }
 }
